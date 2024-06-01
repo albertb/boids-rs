71c685347da6470b677622c6e44d8187098e6826
Albert Bachand <albertb@gmail.com>
2024-06-01 08:37:34 -0400

    move /gh-pages to /docs

--- build_wasm.sh
+++ build_wasm.sh
@@ -2,5 +2,5 @@
 
 cargo build --release --target wasm32-unknown-unknown && \
 wasm-bindgen --no-typescript --target web \
---out-dir ./gh-pages --out-name boids \
+--out-dir ./docs --out-name boids \
 ./target/wasm32-unknown-unknown/release/boids.wasm
